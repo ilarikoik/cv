0685ec4eb152c37b085be122aae742c57e71c6e4
ilarikoik <ilari.koik@gmail.com>
2026-08-27 19:54:06 +0300

    env fix

--- app/api/projectClicks/route.ts
+++ app/api/projectClicks/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: Request) {
     const forwarded = request.headers.get("x-forwarded-for");
     const ip = forwarded ? forwarded.split(",")[0] : "unknown";
 
-    
+   
     if (ip === process.env.MY_IP) {
         console.log("OMA IP, skip updating clicks");
         return NextResponse.json({ skipped: true }, { status: 200 });
